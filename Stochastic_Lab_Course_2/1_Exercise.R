fd8421c0bf73d822fb4cb60e103f70f69e2fd61d
Trevor <khwamtrevor@gmail.com>
2019-03-06 11:55:44 +0100

    Exercises 1 and 2 solved

--- Stochastic_Lab_Course_2/1_Exercise.R
+++ Stochastic_Lab_Course_2/1_Exercise.R
@@ -1,13 +1,97 @@
 setwd("~/Stochastic_Lab/Stochastic_Lab_Course_2")
 library("tidyverse")
 library("haven")
-
+library(maptools)
+library(raster)
+library(rgdal)
 ##Question(a)
 
 children <- read_dta("childrenfinal.dta")
 #remove all variables that start with “s“, “v” and “m”, followed by a number
 children1<- children %>% 
-  select(-matches("^[svm][0-9]"))
+  dplyr::select(-matches("^[svm][0-9]"))
+
+#convert labelled double into double variables
+children2<- children1 %>%
+  mutate_if(is.double, as.double)
+
+##Question(b)
+
+#(b) Make a smaller tibble that contains variables hypage, ruralfacto, female, zstunt, zweight, zwast, adm2.
+children3<- children2 %>%
+  dplyr::select(c(hypage, ruralfacto, female, zstunt, zweight, zwast, adm2))
+
+#Make a scatter plot of zstunt against hypage
+ggplot(children3, aes(x = zstunt, y = hypage)) +
+  geom_point() +
+  ggtitle("scatter plot of zstunt against hypage")
+
+#Add a smooth line to the plot
+ggplot(children3, aes(x = zstunt, y = hypage)) +
+  geom_point() +
+  geom_smooth(se = F) +
+  ggtitle("scatter plot of zstunt against hypage \n with smooth line")
+
+#smooth plots of zstunt against age for females and males on one plot
+ggplot(children3, aes(x = zstunt, y = hypage,  colour = factor(female))) +
+  geom_point() +
+  geom_smooth(se = F) +
+  scale_colour_manual(labels = c("male", "female"), values = c("blue2", "green3")) +
+  guides(colour = guide_legend(title="Gender")) +
+  ggtitle("scatter plot of zstunt against hypage \n for females and males")
+
+#plot zstunt against age for urban and rural children
+ggplot(children3, aes(x = zstunt, y = hypage,  colour = factor(ruralfacto))) +
+  geom_point() +
+  geom_smooth(se = F) +
+  scale_colour_manual(labels = c("urban", "rural"), values = c("blue2", "green3")) +
+  guides(colour = guide_legend(title="Area")) +
+  ggtitle("zstunt against age for urban and rural children")
+
+#Experiment with different aesthetics, themes and font sizes for the plots, report your favourite
 
 
+##Question(c)
+#help link https://rpubs.com/spoonerf/countrymapggplot2
+Kenya1<-getData("GADM", country="KE", level=1) #Download Kenya shapefile data
 
+#WARNING for linux users: Do not capitalize epsg in the following code to avoid errors.
+Kenya1_UTM<-spTransform(Kenya1, CRS("+init=epsg:32537")) # setting an appropriate projection 
+colnames(children3)[7] <- "NAME_1" #rename adm2 into NAME_1
+
+#sort in alphabetic order with respect to NAME_1
+children3<- children3[order(children3$NAME_1),]
+Kenya1_UTM@data<- Kenya1_UTM@data[order(Kenya1_UTM@data$NAME_1),]
+
+#summarising children3 data by the mean of zstunt in the corresponding county
+children4 <- children3 %>%
+  group_by(NAME_1) %>%
+  summarise(mean = mean(zstunt), n = n())
+
+#Adding the missing county Isiolo
+children4[nrow(children4) + 1,] <- NA
+children4$NAME_1[47] <- "Isiolo"
+
+#Prepare the dataframe for ggplot
+Kenya1_UTM@data$id <- rownames(Kenya1_UTM@data)
+Kenya1_UTM@data <- mutate(Kenya1_UTM@data, zstunt.mean= children4$mean)
+Kenya1_df <- fortify(Kenya1_UTM)
+Kenya1_df <- full_join(Kenya1_df,Kenya1_UTM@data, by="id")
+
+#In order to add names to map, we need another dataframe with all the conunties' centroids
+# "coordinates" extracts centroids of the polygons, in the order listed at Kenya1_UTM@data
+centroids_df <- as.data.frame(coordinates(Kenya1_UTM))
+names(centroids_df) <- c("long", "lat")
+children4<- children4[order(children4$NAME_1),]
+centroids_df$NAME_1 <- Kenya1_UTM@data$NAME_1
+centroids_df$zstunt.mean <- children4$mean
+
+#Generating the map
+ggplot(data = Kenya1_df, aes(x = long, y = lat, group = group, fill = zstunt.mean)) + 
+  geom_polygon(color = "black", size = 0.25) +
+  geom_text(data = centroids_df, aes(x = long, y = lat, label = NAME_1, group = NULL), size = 3) +
+  scale_fill_distiller(name="Zstunt mean for \n each county", palette = "Spectral") +
+  theme(aspect.ratio=1)
+
+##(d)write the tibble from (b) into a text file
+write.table(children3,"children3.txt")
---
+++ Stochastic_Lab_Course_2/2_Exercise.R
@@ -0,0 +1,92 @@
+set.seed(250)
+library("tidyverse")
+
+##Question (a)
+#Switch the default random number generator in R to Wichmann-Hill
+RNGkind(kind = "Wichmann-Hill", normal.kind = NULL)
+
+#Inversion method to simulate a binomial random variable
+N = 1000
+n = 10
+p = 0.4
+u <- runif(N)
+bins <- .bincode(u, breaks = c(0, pbinom(0:10, 10, 0.4)), right = F, include.lowest = T)
+
+bin_inv <- numeric()
+for(i in 1:N){
+  bin_inv[i] <- bins[i]-1
+}
+#Simulation of a binomial random variable by simulating corresponding Bernoulli random variables by inversion method
+bin_bern <- numeric()
+for (i in 1:N){
+  v <- runif(n)
+  bin_bern[i] <- sum(v < p)
+}
+#Simulation of a binomial random variable with rbinom
+bin_rbin <- rbinom(N, n, p)
+
+#Plot the histograms of all three samples on one panel
+bin_inv <- data.frame(bin_inv)
+bin_inv$method <- rep("inverse CDF", 1000)
+colnames(bin_inv) <- c("rand_num", "method")
+
+bin_bern <- data.frame(bin_bern)
+bin_bern$method <- rep("From Bernoulli", 1000)
+colnames(bin_bern) <- c("rand_num", "method")
+
+bin_rbin <- data.frame(bin_rbin)
+bin_rbin$method <- rep("rbinom", 1000)
+colnames(bin_rbin) <- c("rand_num", "method")
+
+df <- rbind(bin_inv, bin_bern, bin_rbin)
+
+ggplot(df, aes(x = rand_num, fill = method)) +
+  geom_histogram( binwidth=.5, position="dodge") +
+  labs(fill = "Simulation \n method", title = "Histograms of all three samples", x = "random numbers")
+
+#Switch the random number generator back to its default
+RNGkind(kind = "default", normal.kind = NULL)
+
+##Question(b)
+f <- function(x){
+  ((2*pi)^(-1/2))*exp(-(x^2)/2)
+}
+
+g <- function(x){
+  (pi*(1 + x^2))^(-1)
+}
+
+#First determine the best value of the constant c, such that f(x) <= g(x)
+x <- -1500:1500
+c <- max(f(x)/g(x))
+
+#10000 standard normal random variables using 
+#the accept-reject method, generating Cauchy distributed random variables using inversion method
+N <- 10000
+j <- 0
+rand_num <- numeric()
+while(length(rand_num) != N){
+  w <- runif(1) #step 1
+  cauchy <- tan((w-(1/2))*pi) #still step 1
+  U <- runif(1) #step 2
+  if(U*c*g(cauchy) <= f(cauchy)){
+    rand_num[j] <- cauchy #step 3
+    j <- j + 1
+  }
+}
+
+#Histogram of the obtained sample with the standard normal density
+k <- rnorm(N)
+df <- data.frame(rand_num, k)
+ggplot(df) +
+  geom_histogram(aes( x = rand_num, y = ..density.., colour = rand_num), colour ="white") +
+  geom_density(aes(x = k), colour = "blue") +
+  ggtitle("Histogram of the obtained sample and plot the standard normal density")
+
+#QQ-plot
+ggplot(data = df, mapping = aes(sample = rand_num)) +
+  stat_qq() 
+
+#it is not possible to simulate from the standard Cauchy density using
+#the accept-reject method, the standard normal candidate density
+#because max(g(x)/f(x)) == Inf
